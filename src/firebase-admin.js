075ad298a6e4c51628cb0881cf52aabb4b6b4778
Yuvraj Dhakate <ydhakate@calibo.com>
2024-12-01 23:50:28 +0530

    test

--- src/firebase-admin.js
+++ src/firebase-admin.js
@@ -1,12 +1,27 @@
 const admin = require("firebase-admin");
+require("dotenv").config();
+
+console.log("process.env", process.env.TYPE);
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.TEMP1,
+  client_x509_cert_url: process.env.Temp2,
+  universe_domain: process.env.Temp3,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://localtestingapp-a6fcc-default-rtdb.firebaseio.com/" // Your Firebase Realtime DB URL
+    databaseURL: "https://localtestingapp-a6fcc-default-rtdb.firebaseio.com/", // Your Firebase Realtime DB URL
   });
 }
 
